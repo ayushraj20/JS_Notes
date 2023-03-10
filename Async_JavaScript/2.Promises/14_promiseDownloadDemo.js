9f1da74a1432ca1afa5511393b616a45d6fecf3d
Ayush Raj <rajrakshal20@gmail.com>
2023-03-10 16:55:10 +0530

    how promise solve IOC

---
+++ Async_JavaScript/2.Promises/14_promiseDownloadDemo.js
@@ -0,0 +1,27 @@
+function download(url) {
+  console.log("started downloading content form ", url);
+  return new Promise(function exec(res, rej) {
+    setTimeout(function p() {
+      console.log("COmpelted downloading data in 5s");
+      const content = "ABCDEF";
+      res(content);
+    }, 5000);
+  });
+}
+
+x = download("www.xyz.com");
+
+x.then(function fulfillHandler1(value) {
+  console.log("Content downloaded is1", value);
+});
+
+/*
+  How Promises solve the problem of Inversion of Control (IOC) ??
+
+  Both egs: 14_promiseDownloadDemo & 13_callbackDownloadDemo serves the same purpose but
+  prev eg it is implemented using callbacks and this eg is implemented using promises.
+  In prev eg we have given the control of calling the 'callback' to 'download' function.
+  'download' function may call the cb() twice or may not call it at all, but here using 
+  promises we have the control.
+
+*/
