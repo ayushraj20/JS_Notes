38b5c4f66e132c183ff2a6f42703839b606817c6
ayushraj20 <rajrakshal20@gmail.com>
2024-01-29 15:25:45 +0530

    added interview questions asked for Walmart Client project

---
+++ test1.js
@@ -0,0 +1,16 @@
+// currying
+// let temp = sum(1)(2)(3)(4);
+// temp();
+
+// function Sum(val) {
+//   Sum(sum + val);
+// }
+// peer component
+
+{
+  /* <ParentComponent/>
+<Child1/>
+<Child2/>
+<Child3/>
+[isOpen, setIsOpen] = useState(false) */
+}
