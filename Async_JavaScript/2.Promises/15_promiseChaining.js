ddc4222615ae2be649f1a06a473af67f6101c164
Ayush Raj <rajrakshal20@gmail.com>
2024-05-24 22:33:14 +0530

    dfb

--- Async_JavaScript/2.Promises/15_promiseChaining.js
+++ Async_JavaScript/2.Promises/15_promiseChaining.js
@@ -3,28 +3,28 @@
 */
 
 function download(url) {
-  console.log("started downloading content form ", url);
+  console.log('started downloading content form ', url);
   return new Promise(function exec(res, rej) {
     setTimeout(function p() {
-      console.log("COmpelted downloading data in 5s");
-      const content = "ABCDEF";
+      console.log('Completed downloading data in 5s');
+      const content = 'ABCDEF';
       res(content);
     }, 5000);
   });
 }
 
-x = download("www.xyz.com");
+x = download('www.xyz.com');
 
 x.then(function fulfillHandler1(value) {
-  console.log("Content downloaded is1", value);
-  return "New promise string";
+  console.log('Content downloaded is', value);
+  return 'New promise string';
 }).then(function newFullFillHandler(value) {
-  console.log("value from chained then promsie", value);
+  console.log('value from chained .then promise', value);
 });
 
 /*
-  .then() also returns a 'New Promise Object' with state same as parent promise object (here fullfilled)
-  and value is the return of respective handler (here fullfillhandler1) and return is 'New promise string'
+  .then() also returns a 'New Promise Object' with state same as parent promise object (here fulfilled)
+  and value is the return of respective handler (here fulfillhandler1) and return is 'New promise string'
   i.e the value of our new promise object returned by .then() 
 
 */
--- Async_JavaScript/2.Promises/18_callbackImpl.js
+++ Async_JavaScript/2.Promises/18_callbackImpl.js
@@ -1,17 +1,17 @@
 // Tasks: (Don't use promises only use callbacks)
 // 1. Write a function to download data from a url
 // 2. Write a function to save that downloaded data in a file and return the filename
-// 3. Write a function to upload the file written in previous step to a newurl
+// 3. Write a function to upload the file written in previous step to a newUrl
 
 function download(url, cb) {
   /**
    * Downloads content from the given url and passed the
    * downloaded content to the given callback cb
    */
-  console.log("Starting to download data from", url);
+  console.log('Starting to download data from', url);
   setTimeout(function down() {
-    console.log("Downloading completed");
-    const content = "ABCDEF"; // assume dummy download content
+    console.log('Downloading completed');
+    const content = 'ABCDEF'; // assume dummy download content
     cb(content);
   }, 10000);
 }
@@ -20,10 +20,10 @@ function writeFile(data, cb) {
   /**
    * writes the given data into a new file
    */
-  console.log("Started writing a file with", data);
-  setTimeout(function wrtie() {
-    console.log("Completed writing the data in a file");
-    const filename = "file.txt";
+  console.log('Started writing a file with', data);
+  setTimeout(function write() {
+    console.log('Completed writing the data in a file');
+    const filename = 'file.txt';
     cb(filename);
   }, 5000);
 }
@@ -32,10 +32,10 @@ function upload(url, file, cb) {
   /**
    * uploads the data from a file to a given url
    */
-  console.log("Started uploading", file, "on", url);
+  console.log('Started uploading', file, 'on', url);
   setTimeout(function up() {
-    console.log("upload completed");
-    const response = "SUCCESS";
+    console.log('upload completed');
+    const response = 'SUCCESS';
     cb(response);
   }, 2000);
 }
@@ -43,12 +43,12 @@ function upload(url, file, cb) {
 // callback hell -> one callback inside another cb --> callback nesting.
 // below is the eg: of callback hell.
 
-download("www.xyz.com", function processDownload(content) {
-  console.log("We are now going to process the downloaded data");
+download('www.xyz.com', function processDownload(content) {
+  console.log('We are now going to process the downloaded data');
   writeFile(content, function processWrite(filename) {
-    console.log("We have downloaded and written the file, now will upload");
-    upload("www.upload.com", filename, function processUpload(response) {
-      console.log("we have uploaded with", response);
+    console.log('We have downloaded and written the file, now will upload');
+    upload('www.upload.com', filename, function processUpload(response) {
+      console.log('we have uploaded with', response);
     });
   });
 });
