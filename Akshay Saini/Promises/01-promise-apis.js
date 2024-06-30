268e18dad777c1bdb5363702b2192c3549a2dd7a
Ayush Raj <rajrakshal20@gmail.com>
2024-06-30 22:00:12 +0530

    updated

--- Akshay Saini/Promises/01-promise-apis.js
+++ Akshay Saini/Promises/01-promise-apis.js
@@ -7,7 +7,7 @@ Below promise api's take array of promised as an input and returns some value.
                               If any one of them fails it immediately return with the failed err.
 
 2. Promise.allSettled([p1,p2,p3]) -> same as Promise.all() i.e, waits for all of promises(p1,p2,p3) to resolve then returns the array of values.
-                                     But if any one of them fails , It doesn't returns immediately, it waits for all of the promises
+                                     But if any one of them fails, It doesn't returns immediately, it waits for all of the promises
                                      to get settled and then returns.
 
 3. Promise.race([p1,p2,p3]) -> waits for 1st promise to settle(resolve or reject), and returns its value.
