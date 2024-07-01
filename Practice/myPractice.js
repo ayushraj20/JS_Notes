19c87264fc3b0b47f26d9fb24d68053df80a8cb0
Ayush Raj <rajrakshal20@gmail.com>
2024-07-01 11:19:10 +0530

    fcs

--- Practice/myPractice.js
+++ Practice/myPractice.js
@@ -1,10 +1,39 @@
-function add(a) {
-  return function (b) {
-    if (b) {
-      return add(a + b);
-    } else return a;
-  };
-}
+let p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('P1 resolved');
+  }, 1000);
+});
 
-const result = add(1)(2)(3)(1)(3)();
-console.log(result);
+let p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('P2 resolved');
+  }, 3000);
+});
+
+// Promise.all([p1, p2])
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => console.log(err));
+
+Promise.myAll = function (promiseArr) {
+  let resArr = [];
+  let counter = 0;
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < promiseArr.length; i++) {
+      promiseArr[i].then((res) => {
+        counter++;
+        resArr.push(res);
+        if (counter === promiseArr.length) {
+          resolve(resArr);
+        }
+      });
+    }
+  });
+};
+
+Promise.myAll([p1, p2])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => console.log(err));
