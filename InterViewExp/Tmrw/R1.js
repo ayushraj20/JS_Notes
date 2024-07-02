5e4ee6f61d6cbcd31eb072f91e9ce697df747700
Ayush Raj <rajrakshal20@gmail.com>
2024-07-02 19:52:46 +0530

    added

---
+++ InterViewExp/Tmrw/R1.js
@@ -0,0 +1,17 @@
+/*
+ Tmrw - House of Brands SDE-1 (FrontEnd)
+
+ Key TakeAway - You Should know every line written in your Resume in-Detail.
+ 
+ 
+ 1. How Did you Implemented 'SSG' and 'SSR' in NextJS as you have written in your resume ?
+
+ 2. How have you implemented 'Lazy Loading', what does 'Suspense' do ?
+
+ 3. What would happen to redux values if you reload the page, How will you manage cart functionality,
+    If it is not a SPA (Single Page Application) ?
+
+ 4. What is 'useIntersectionObserver' and its uses ? // Throttling
+
+
+*/
