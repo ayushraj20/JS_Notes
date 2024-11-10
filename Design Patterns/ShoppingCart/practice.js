a5cd2a7fa537beb6aa3ceee8a7adbc45353b2474
ayushraj20 <rajrakshal20@gmail.com>
2024-11-10 21:50:45 +0530

    dfh

--- Design Patterns/ShoppingCart/practice.js
+++ Design Patterns/ShoppingCart/practice.js
@@ -23,6 +23,7 @@ class Cart {
 
   addItem(item) {
     this.items.push(item);
+    // console.log('items', this.items);
   }
 
   calculateTotal() {
@@ -34,16 +35,76 @@ class Cart {
       if (item instanceof Coupon) {
         switch (item.type) {
           case 'percentAll':
-            nextDiscountPercent = 1 - item.value / 100;
+            allDiscountPercent = 1 - item.value / 100;
             break;
 
           case 'percentNext':
-            allDiscountPercent = 1 - item.value / 100;
+            nextDiscountPercent = 1 - item.value / 100;
             break;
+
+          case 'fixedNth':
+            if (!this.productCount[item.productType]) {
+              this.productCount[item.productType] = 0;
+            }
         }
       } else if (item instanceof Product) {
+        let productPrice = item.price;
+
+        productPrice *= allDiscountPercent;
+
+        productPrice *= nextDiscountPercent;
+        nextDiscountPercent = 1.0;
+
+        if (!this.productCount[item.name]) {
+          this.productCount[item.name] = 0;
+        }
+        this.productCount[item.name] += 1;
+
+        for (let discount of this.items) {
+          if (
+            discount instanceof Coupon &&
+            discount.type === 'fixedNth' &&
+            discount.productType === item.name &&
+            discount.productIndex === this.productCount[item.name]
+          ) {
+            productPrice -= discount.value;
+          }
+        }
+
+        totalPrice += Math.max(productPrice, 0);
       }
     }
-    return this.totalPrice;
+
+    this.finalPrice = totalPrice;
+    return this.finalPrice;
   }
 }
+
+// Example Usage:
+
+let cart = new Cart();
+
+//TestCase - 1
+// cart.addItem(new Coupon('percentNext', 10));
+// cart.addItem(new Product('postcard sorter', 10));
+// cart.addItem(new Product('stationery organizer', 20));
+
+//TestCase - 2
+// cart.addItem(new Product('postcard sorter', 10));
+// cart.addItem(new Coupon('percentNext', 10));
+// cart.addItem(new Product('stationery organizer', 20));
+
+// TestCase - 3
+cart.addItem(new Coupon('percentAll', 5)); // 5% off all products
+cart.addItem(new Product('postcard sorter', 10));
+cart.addItem(new Product('stationery organizer', 15));
+cart.addItem(new Coupon('fixedNth', 5, 'postcard sorter', 3)); // $5 off 3rd postcard sorter
+cart.addItem(new Coupon('percentNext', 50)); // 50% off next product
+cart.addItem(new Product('postcard sorter', 10));
+cart.addItem(new Product('business card holder', 18));
+cart.addItem(new Coupon('fixedNth', 2, 'postcard sorter', 2)); // $2 off 2nd postcard sorter
+cart.addItem(new Product('postcard sorter', 16));
+cart.addItem(new Product('postcard sorter', 10));
+
+// Calculate total
+console.log('Total Price:', cart.calculateTotal());
