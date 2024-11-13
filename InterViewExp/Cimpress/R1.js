03f0072508499eee9218008a903faee9135ae03b
ayushraj20 <rajrakshal20@gmail.com>
2024-11-13 15:10:59 +0530

    fh

---
+++ InterViewExp/Cimpress/R1.js
@@ -0,0 +1,19 @@
+/*
+   DSA -
+    1. Check if number is even or Odd without any conditional Statement.
+    2. Longest Substring Without Repeating Characters (Sliding Window).
+
+*/
+
+// console.log('23' + '10' + '3' + 2 * 5);
+// console.log('23' + '10' + '3' + 2 - 5);
+
+// call(), apply(), bind(), promise.All(), promise.allSettled()
+
+/*
+Declare and arrow function in TypeScript , I missed the function return type
+const myFunction = (param1: string, param2: number): boolean => {
+  return true;
+};
+
+*/
